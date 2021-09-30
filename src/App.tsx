60d314403808e6b3874a06235673ad9478046cc4
Ivo <example@example.com>
2021-09-30 11:58:50 +0100

    Added support for css and other assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import "./styles.css"
+import IMAGE from "./public/bugfix.svg"
 const App = () => {
     return(
         <>
             <h1>React TypeScript</h1>
+            <img src={IMAGE} alt="space apple" width="300" height="300"/>
         </>
     );
 }
